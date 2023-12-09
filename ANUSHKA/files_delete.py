9790ab103f93a00595f691e2471502f3278fe6dc
ANUSHKA SEN <153029801+anushka-sen-bot@users.noreply.github.com>
2023-12-09 19:40:43 +0100

    Update files_delete.py

--- ANUSHKA/files_delete.py
+++ ANUSHKA/files_delete.py
@@ -2,7 +2,7 @@ import re
 import logging
 from pyrogram import Client, filters
 from info import DELETE_CHANNELS
-from database.ia_filterdb import Media, unpack_new_file_id
+from database.ANUSHKADB.ia_filterdb import Media, unpack_new_file_id
 
 logger = logging.getLogger(__name__)
 
