10af76700464fa76571e472bb30fb741855a331c
ANUSHKA SEN <153029801+anushka-sen-bot@users.noreply.github.com>
2023-12-09 19:47:03 +0100

    Update inline.py

--- ANUSHKA/inline.py
+++ ANUSHKA/inline.py
@@ -2,10 +2,10 @@ import logging
 from pyrogram import Client, emoji, filters
 from pyrogram.errors.exceptions.bad_request_400 import QueryIdInvalid
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument, InlineQuery
-from database.ia_filterdb import get_search_results
+from database.ANUSHKADB.ia_filterdb import get_search_results
 from utils import is_subscribed, get_size, temp
 from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION, REQ_CHANNEL
-from database.connections_mdb import active_connection
+from database.ANUSHKADB.connections_mdb import active_connection
 
 logger = logging.getLogger(__name__)
 cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
