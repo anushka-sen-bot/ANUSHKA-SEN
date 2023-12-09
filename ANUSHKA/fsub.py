9bb92520c0a4ab30ed9a85cfe6e9a1025c405411
ANUSHKA SEN <153029801+anushka-sen-bot@users.noreply.github.com>
2023-12-09 19:42:15 +0100

    Update fsub.py

--- ANUSHKA/fsub.py
+++ ANUSHKA/fsub.py
@@ -3,7 +3,7 @@ from pyrogram import Client, enums
 from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
-from database.join_reqs import JoinReqs
+from database.ANUSHKADB.join_reqs import JoinReqs
 from info import REQ_CHANNEL, AUTH_CHANNEL, JOIN_REQS_DB, ADMINS
 
 from logging import getLogger
