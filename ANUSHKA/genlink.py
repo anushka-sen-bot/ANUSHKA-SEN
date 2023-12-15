0cdc31932214917d6029ebe69b440546d22df21c
ANUSHKA SEN <153029801+anushka-sen-bot@users.noreply.github.com>
2023-12-16 00:38:52 +0100

    Update genlink.py

--- ANUSHKA/genlink.py
+++ ANUSHKA/genlink.py
@@ -1,7 +1,7 @@
 import re
 from pyrogram import filters, Client, enums
 from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, UsernameInvalid, UsernameNotModified
-from info import ADMINS, LOG_CHANNEL, FILE_STORE_CHANNEL, PUBLIC_FILE_STORE
+from ANUSHKA.info import ADMINS, LOG_CHANNEL, FILE_STORE_CHANNEL, PUBLIC_FILE_STORE
 from database.ANUSHKADB.ia_filterdb import unpack_new_file_id
 from ANUSHKA.utils import temp
 import re
