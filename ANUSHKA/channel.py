1a5ff0a813b1b218f5b138d1395b99a59da602be
ANUSHKA SEN <153029801+anushka-sen-bot@users.noreply.github.com>
2023-12-16 00:42:10 +0100

    Update channel.py

--- ANUSHKA/channel.py
+++ ANUSHKA/channel.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from info import CHANNELS
+from ANUSHKA.info import CHANNELS
 from database.ANUSHKADB.ia_filterdb import save_file
 
 media_filter = filters.document | filters.video | filters.audio
