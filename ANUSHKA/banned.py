31375ff73482a87983733698d5cc443fb7c6b6dd
ANUSHKA SEN <153029801+anushka-sen-bot@users.noreply.github.com>
2023-12-16 00:43:06 +0100

    Update banned.py

--- ANUSHKA/banned.py
+++ ANUSHKA/banned.py
@@ -3,7 +3,7 @@ from ANUSHKA.utils import temp
 from pyrogram.types import Message
 from database.ANUSHKADB.users_chats_db import dbanushka as db
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from info import SUPPORT_CHAT
+from ANUSHKA.info import SUPPORT_CHAT
 
 async def banned_users(_, client, message: Message):
     return (
