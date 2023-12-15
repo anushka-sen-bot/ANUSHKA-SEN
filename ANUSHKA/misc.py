07ce6b0dd302bfc99e285f3e7b6d130c59240fce
ANUSHKA SEN <153029801+anushka-sen-bot@users.noreply.github.com>
2023-12-16 00:36:13 +0100

    Update misc.py

--- ANUSHKA/misc.py
+++ ANUSHKA/misc.py
@@ -1,7 +1,7 @@
 import os
 from pyrogram import Client, filters, enums
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
-from info import IMDB_TEMPLATE
+from ANUSHKA.info import IMDB_TEMPLATE
 from ANUSHKA.utils import extract_user, get_file_id, get_poster, last_online
 import time
 from datetime import datetime
