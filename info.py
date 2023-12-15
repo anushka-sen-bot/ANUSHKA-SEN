d757eb4b24128c7a5fca55228487dcd2b1552200
ANUSHKA SEN <153029801+anushka-sen-bot@users.noreply.github.com>
2023-12-16 00:30:20 +0100

    Update info.py

--- info.py
+++ info.py
@@ -82,7 +82,7 @@ COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 ANUSHKA_COMMAND_HAND_LER = environ.get("ANUSHKA_COMMAND_HAND_LER", "/")
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Nobideveloper")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Freya")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # 2nd Bot DB
